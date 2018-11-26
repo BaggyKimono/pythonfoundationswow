9f7a22702052a7ed45836454c4af797cd3c6b682
BaggyKimono <allymweatherly@gmail.com>
2018-11-26 14:45:18 -0500

    yup printing

--- 07Errors and exceptions.py
+++ 07Errors and exceptions.py
@@ -6,4 +6,17 @@ date: 11/26/18 2:03 PM
 
 import random
 
+try:
+    eval('x === x')
+except SyntaxError:
+    print("You cannot do that")
+
+
+print(10 + (1 / 0))
+try:
+    print(10 + (1 / 0))
+except ZeroDivisionError:
+    print("Oops! We can't divide by zero, please choose a different number and re-evaluate.")
+
+
 
---
+++ 08boolean.py
@@ -0,0 +1,21 @@
+"""
+title: 08boolean
+author: Ally
+date: 11/26/18 2:20 PM
+"""
+
+import random
+
+
+
+
+
+print(True and True)
+print(False and True)
+print(1 == 1 and 2 == 1)
+print("test" == "test")
+print(1 == 1 or 2 != 1)
+print(True and 1 == 1)
+print(False and 0 != 0)
+print(True or 1 == 1)
+

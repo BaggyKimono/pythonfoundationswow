0bfe1f893020559bddc962e708b524ad2a0ba1fe
BaggyKimono <allymweatherly@gmail.com>
2018-11-28 13:43:10 -0500

    Learning the things

---
+++ 13 DS - Tuples.py
@@ -0,0 +1,44 @@
+"""
+title: 13 DS - Tuples
+author: Ally
+date: 11/28/18 1:31 PM
+"""
+
+"""_________________Data Structures ___________________________"""
+
+"""
+
+It is very common to want to list numbers or strings. This is possible with data structures. 
+Data structures are a specialized format for organizing and storing data. 
+There are quite a few data structures that are available that will be described in the following sections.
+
+"""
+
+"""_________________Tuples___________________________"""
+
+"""
+A tuple is a data structure in Python that is an immutable, or unchangeable, ordered sequence of elements. 
+Because it is immutable, the elements in the tuple cannot be changed. 
+The immutability of a tuple helps send the message to others that the information inside of the tuple should not change.
+
+Each element or value that is inside of a tuple is called an item. 
+Just as lists are defined as items between square brackets, tuples are 
+defined by having values between parenthesis ( ) and separated by commas. Empty tuples look like: grade_level = (), 
+but even tuples with just one item must use commas as in grade_level = (9, )
+
+."""
+
+"""
+2. Tuples
+2.1. Length
+2.2. Indexing
+2.3. Slicing
+2.4. Concatenating
+2.5. Tuples vs. Lists
+2.6. Applying your knowledge
+2.7. Tuples Exercises
+
+"""
+
+
+

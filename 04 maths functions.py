27ea4f549a14552ab8e5e1627c08035d600faf40
BaggyKimono <allymweatherly@gmail.com>
2018-11-28 14:32:53 -0500

    Learning the things

--- 04 maths functions.py
+++ 04 maths functions.py
@@ -24,6 +24,7 @@ Arithmetic operators are used to perform mathematical operations like addition, 
                     Table 1. Arithmetic Operators
 
                     Name	              Operator
+
                     Addition                 +
                     Subtraction              -
                     Multiplication           *
@@ -34,47 +35,82 @@ Arithmetic operators are used to perform mathematical operations like addition, 
                     Exponent                **
 
 
-Open IDLE by typing command + spacebar, type idle and hit return.
-Python is an interpreted language, and we can use the Python Console to type any numbers or commands just like a calculator.
+# Open IDLE by typing command + spacebar, type idle and hit return.
+# Python is an interpreted language, and we can use the Python Console
+# to type any numbers or commands just like a calculator.
 
-Try typing a few numbers, and view the output.
+# Try typing a few numbers, and view the output.
 
-Python 2 treated division differently: the / symbol would default to integer division.
-Now, let’s try a few expressions with some of the arithmetic operators:
+            Python 2 treated division differently: the / symbol would default to integer division.
 
-1
-2
-3
-4
-5
-6
-7
-35 + 5
-45 - 5
-8 * 5
-400 / 10
-400 // 10
--35 + -5
-40 % 10000
-Each of these expressions should evaluate to 40 in your python shell (except for the negation expression, which should evaluate to -40)
+# Now, let’s try a few expressions with some of the arithmetic operators:
 
-One thing to note is that when using the / operator, the result will return as a float. In the above example, 400 / 10 evaluates to 40.0 If you want a whole number returned, use the // double-slash operator. Just remember that whenever you see a number without a decimal point, python is interpreting it as an int, and when there is a number with a decimal point, it’s a float. This is important to note for now, because as we move on to learning about loops we’ll realize that Python is opinionated on which operator can be used in specific situations.
 
-Also, let’s talk about the remainder operator, also called modulus. This will return a whole number which will be the remainder of the expression. For example, 10 % 3 will output 1, since 1 is the remainder when you divide 10 by 3
+            35 + 5
+            45 - 5
+            8 * 5
+            400 / 10
+            400 // 10
+            -35 + -5
+            40 % 10000
 
-1.2. Operator precedence
-All operators are not equal. Remember the PEMDAS rule from math class! Let’s take a look.
+# Each of these expressions should evaluate to 40 in your python shell
+# (except for the negation expression, which should evaluate to -40)
 
-When you take our expressions from the previous example, and put them into a more complex expression, we can see how operator precedence works.
+        One thing to note is that when using the / operator, the result will return as a float.
 
-Type the following into IDLE:
 
-10 + 5 / 3 - 4 * 12
-If you go through this expression in order, as 10 + 5 is 15, dividing by 3 gives us 5 and subtracting 4 leaves us with 1 before dividing by 12. You may be lead to believe the answer is 12 when it is in fact -36.333333333333336 (We see the float because we used the single / divisor)
+!!!!------------------------------------------------------------------------------------------------------ !!!!
+        In the above example, 400 / 10 evaluates to 40.0
+        If you want a whole number returned, use the // double-slash operator.
+!!!!------------------------------------------------------------------------------------------------------ !!!!
 
-When you look at an expression like this and use PEMDAS, we have to first use multiplication to get 4 * 12 = 48, then take the value for b and do 3/3 which is 1.0. Then add 12 + 1.0, and finally 13.0 - 48 which gives us -36.333333333333336
+
+!!!!------------------------------------------------------------------------------------------------------ !!!!
+        Just remember that whenever you see a number without a decimal point, python is
+        interpreting it as an int, and when there is a number with a decimal point, it’s a float.
+!!!!------------------------------------------------------------------------------------------------------!!!!
+
+
+
+        This is important to note for now, because as we move on to learning about loops we’ll realize that
+        Python is opinionated on which operator can be used in specific situations.
+
+
+
+!!!!------------------------------------------------------------------------------------------------------!!!!
+        Also, let’s talk about the remainder operator, also called modulus.
+        This will return a whole number which will be the remainder of the expression.
+        For example, 10 % 3 will output 1, since 1 is the remainder when you divide 10 by 3
+!!!!------------------------------------------------------------------------------------------------------!!!!
+
+
+
+
+
+"""-------------1.2. Operator precedence------------------"""
+
+# All operators are not equal. Remember the PEMDAS rule from math class! Let’s take a look.
+
+            # When you take our expressions from the previous example, and put them
+            # into a more complex expression, we can see how operator precedence works.
+
+    Type the following into IDLE:
+
+            10 + 5 / 3 - 4 * 12
+
+           # If you go through this expression in order, as 10 + 5 is 15, dividing by 3 gives us
+           # 5 and subtracting 4 leaves us with 1 before dividing by 12. You may be lead to believe
+           # the answer is 12 when it is in fact -36.333333333333336 (We see the float because we
+           # used the single / divisor)
+
+    When you look at an expression like this and use PEMDAS, we have to first use multiplication to
+    get 4 * 12 = 48, then take the value for b and do 3/3 which is 1.0. Then add 12 + 1.0,
+    and finally 13.0 - 48 which gives us -36.333333333333336
 
 Put your knowledge to work!
+
+
 
 Using the same operators and numbers as the previous example, what does the following expression evaluate to when you put it into IDLE?
 
